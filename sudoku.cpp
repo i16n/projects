a1615fdbc8685ad6a32937da47496defa688c45c
Isaac Huntsman <105136275+i16n@users.noreply.github.com>
2024-12-03 20:45:28 -0700

    Redo includes, rename class

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <optional>
-#include <set>
-#include <algorithm>
 #include <random>
-#include <ctime>
 using namespace std;
 
 vector<vector<char>> solutionBoard;
 
-vector<char> generateSeed() {
+vector<char> generateSeed()
+{
     vector<char> digits = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     // seed an rng
@@ -22,20 +18,24 @@ vector<char> generateSeed() {
 }
 
 // generateStartPos method written by chatGPT o1-preview
-vector<vector<bool>> generateStartPos(int clueNums) {
+vector<vector<bool>> generateStartPos(int clueNums)
+{
     // Step 1: Initialize the 9x9 grid with integer characters
     vector<vector<char>> grid(9, vector<char>(9));
 
     // fill with nums 1 through 9
-    for (int i = 0; i < 9; ++i) {
-        for (int j = 0; j < 9; ++j) {
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
             grid[i][j] = '1' + (i + j) % 9;
         }
     }
 
     // generate positions from 0 to 80
     vector<int> positions(81);
-    for (int i = 0; i < 81; ++i) {
+    for (int i = 0; i < 81; ++i)
+    {
         positions[i] = i;
     }
 
@@ -48,7 +48,8 @@ vector<vector<bool>> generateStartPos(int clueNums) {
     // then fetch first x elements from positions, a shuffled vector<int>
     // flip those elements in _show_ to false
     vector<vector<bool>> show(9, vector<bool>(9, false));
-    for (int k = 0; k < clueNums; ++k) {
+    for (int k = 0; k < clueNums; ++k)
+    {
         int p = positions[k];
         int row = p / 9;
         int col = p % 9;
@@ -58,130 +59,166 @@ vector<vector<bool>> generateStartPos(int clueNums) {
     return show;
 }
 
-class GenerateBoard {
+class Board
+{
 
-    public:
-        void generateSudoku(vector<vector<char>>& board) {
-            backtrack(board, 0, 0);
+public:
+    void generateSudoku(vector<vector<char>> &board)
+    {
+        backtrack(board, 0, 0);
+    }
+
+    bool isValid(const vector<vector<char>> &board, int row, int col, char num)
+    {
+        // Check the row
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[row][i] == num)
+                return false;
         }
-
-        bool isValid(const vector<vector<char>>& board, int row, int col, char num) {
-            // Check the row
-            for (int i = 0; i < 9; i++) {
-                if (board[row][i] == num) return false;
-            }
-            // Check the column
-            for (int i = 0; i < 9; i++) {
-                if (board[i][col] == num) return false;
-            }
-            // Check the 3x3 sub-box
-            int boxRowStart = 3 * (row / 3);
-            int boxColStart = 3 * (col / 3);
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (board[boxRowStart + i][boxColStart + j] == num) {
-                        return false;
-                    }
+        // Check the column
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[i][col] == num)
+                return false;
+        }
+        // Check the 3x3 sub-box
+        int boxRowStart = 3 * (row / 3);
+        int boxColStart = 3 * (col / 3);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[boxRowStart + i][boxColStart + j] == num)
+                {
+                    return false;
                 }
             }
-            return true;
         }
+        return true;
+    }
 
-        // passing board by reference. We need to actually wipe away vals besides our 20
-        void Render(vector<vector<char>>& board, vector<vector<bool>> itsaStartingPos, bool isInitialRender) {
+    // passing board by reference. We need to actually wipe away vals besides our 20
+    void Render(vector<vector<char>> &board, vector<vector<bool>> itsaStartingPos, bool isInitialRender)
+    {
 
-            cout << "    1 2 3 4 5 6 7 8 9" << endl;
-            cout << "    - - - - - - - - -" << endl;
+        cout << "    1 2 3 4 5 6 7 8 9" << endl;
+        cout << "    - - - - - - - - -" << endl;
 
-            for (int i = 0; i < board.size(); i++) {
-                
-                // draw row lettering
-                char currRow = 'a' + i;
-                cout << currRow << " | ";
-                
-                for (int j = 0; j < board[i].size(); j++) {
-                    if (itsaStartingPos[i][j]) {
-                        // Display starting position in red
-                        cout << "\033[31m" << board[i][j] << "\033[0m" << " ";
-                    } else {
-                        if (isInitialRender) {
-                            // Clear mutable cells for initial rendering
-                            board[i][j] = ' ';
+        for (int i = 0; i < board.size(); i++)
+        {
+
+            // draw row lettering
+            char currRow = 'a' + i;
+            cout << currRow << " | ";
+
+            for (int j = 0; j < board[i].size(); j++)
+            {
+                if (itsaStartingPos[i][j])
+                {
+                    // Display starting position in red
+                    cout << "\033[31m" << board[i][j] << "\033[0m" << " ";
+                }
+                else
+                {
+                    if (isInitialRender)
+                    {
+                        // Clear mutable cells for initial rendering
+                        board[i][j] = ' ';
+                        cout << "  ";
+                    }
+                    else
+                    {
+                        // Display non-empty mutable cells for re-rendering
+                        if (board[i][j] != ' ')
+                        {
+                            cout << board[i][j] << " ";
+                        }
+                        else
+                        {
                             cout << "  ";
-                        } else {
-                            // Display non-empty mutable cells for re-rendering
-                            if (board[i][j] != ' ') {
-                                cout << board[i][j] << " ";
-                            } else {
-                                cout << "  ";
-                            }
                         }
                     }
                 }
-                cout << endl;
+            }
+            cout << endl;
+        }
+    }
+
+private:
+    bool backtrack(vector<vector<char>> &board, int row, int col)
+    {
+        auto openPos = findNextOpenPos(board, row, col);
+        if (!openPos.has_value())
+            return true;
+
+        int openRow = openPos->first;
+        int openCol = openPos->second;
+
+        for (char num = '1'; num <= '9'; num++)
+        {
+            if (isValid(board, openRow, openCol, num))
+            {
+                board[openRow][openCol] = num;
+                if (backtrack(board, openRow, openCol))
+                    return true;               // Solution found!
+                board[openRow][openCol] = ' '; // Undo move
             }
         }
+        return false; // Trigger backtracking
+    }
 
-
-    private:
-        bool backtrack(vector<vector<char>>& board, int row, int col) {
-            auto openPos = findNextOpenPos(board, row, col);
-            if (!openPos.has_value()) return true;
-
-            int openRow = openPos->first;
-            int openCol = openPos->second;
-
-            for (char num = '1'; num <= '9'; num++) {
-                if (isValid(board, openRow, openCol, num)) {
-                    board[openRow][openCol] = num;
-                    if (backtrack(board, openRow, openCol)) return true; // Solution found!
-                    board[openRow][openCol] = ' '; // Undo move
+    optional<pair<int, int>> findNextOpenPos(const vector<vector<char>> &board, int currRow, int currCol)
+    {
+        for (int i = currRow; i < 9; i++)
+        {
+            for (int j = (i == currRow ? currCol : 0); j < 9; j++)
+            {
+                if (board[i][j] == ' ')
+                {
+                    return make_pair(i, j);
                 }
             }
-            return false; // Trigger backtracking
         }
-
-        optional<pair<int, int>> findNextOpenPos(const vector<vector<char>>& board, int currRow, int currCol) {
-            for (int i = currRow; i < 9; i++) {
-                for (int j = (i == currRow ? currCol : 0); j < 9; j++) {
-                    if (board[i][j] == ' ') {
-                        return make_pair(i, j);
-                    }
-                }
-            }
-            return nullopt; // No open position found
-        }
+        return nullopt; // No open position found
+    }
 };
 
 // pass board by reference
-bool takeATurn(vector<vector<char>>& board, vector<vector<bool>> startingPositions) {
-    GenerateBoard gameBoard;
+bool takeATurn(vector<vector<char>> &board, vector<vector<bool>> startingPositions)
+{
+    Board gameBoard;
     string userChoice;
-    cout << endl <<  "Enter row,col to write number, like \"a1\": ";
+    cout << endl
+         << "Enter row,col to write number, like \"a1\": ";
     cin >> userChoice;
 
-    if (userChoice == "s") {
+    if (userChoice == "s")
+    {
         // show solved board.
         gameBoard.Render(solutionBoard, startingPositions, false);
         cout << endl;
         return true;
     }
 
-    if (userChoice.length() != 2) {
+    if (userChoice.length() != 2)
+    {
         cout << "input can only be length 2." << endl;
         return false;
     }
 
     // here userChoice guaranteed to be length 2. Safe to index like so.
-    int userRowConverted = userChoice[0]-'a';
-    int userColConverted = userChoice[1]-49;
+    int userRowConverted = userChoice[0] - 'a';
+    int userColConverted = userChoice[1] - 49;
 
-    if (userRowConverted > 8 || userRowConverted < 0 || userColConverted > 8 || userColConverted < 0) {
+    if (userRowConverted > 8 || userRowConverted < 0 || userColConverted > 8 || userColConverted < 0)
+    {
         cout << "invalid row or column" << endl;
         return false;
     }
 
-    if (startingPositions[userRowConverted][userColConverted]) {
+    if (startingPositions[userRowConverted][userColConverted])
+    {
         cout << "can't overwrite a starting clue." << endl;
         return false;
     }
@@ -193,13 +230,15 @@ bool takeATurn(vector<vector<char>>& board, vector<vector<bool>> startingPositio
     cin >> potentialNum;
 
     // allow user to input a '.' -> delete char at that position.
-    if (potentialNum == ".") {
+    if (potentialNum == ".")
+    {
         // board augment
         board[userRowConverted][userColConverted] = ' ';
         return true;
     }
 
-    if (potentialNum == "s") {
+    if (potentialNum == "s")
+    {
         // show solved board.
         gameBoard.Render(solutionBoard, startingPositions, false);
         cout << endl;
@@ -207,26 +246,31 @@ bool takeATurn(vector<vector<char>>& board, vector<vector<bool>> startingPositio
     }
 
     int num;
-    try {
+    try
+    {
         num = stoi(potentialNum);
     }
-    catch (const invalid_argument& e) {
+    catch (const invalid_argument &e)
+    {
         cout << "not a number, try again" << endl;
         return false;
     }
-    catch (const out_of_range& e) {
+    catch (const out_of_range &e)
+    {
         cout << "number too big, try again" << endl;
         return false;
     }
 
-    if (num < 1 || num > 9) {
+    if (num < 1 || num > 9)
+    {
         cout << "number must be in [1-9]" << endl;
         return false;
     }
 
     char character = static_cast<char>(num + '0');
-    
-    if (!gameBoard.isValid(board, userRowConverted,userColConverted,character)) {
+
+    if (!gameBoard.isValid(board, userRowConverted, userColConverted, character))
+    {
         cout << "number already in row, column, or box." << endl;
         return false;
     }
@@ -237,7 +281,8 @@ bool takeATurn(vector<vector<char>>& board, vector<vector<bool>> startingPositio
     return true;
 }
 
-int main() {
+int main()
+{
 
     vector<vector<char>> board(9, vector<char>(9, 0));
     vector<vector<char>> boardDeepCopy(9, vector<char>(9, 0));
@@ -246,13 +291,17 @@ int main() {
     vector<char> seed = generateSeed();
 
     // build seed board
-    for (int i = 0; i < board.size(); i++) {
-        for (int j = 0 ; j < board.size(); j++) {
-            if (i == 0) {
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board.size(); j++)
+        {
+            if (i == 0)
+            {
                 board[i][j] = seed[j];
                 boardDeepCopy[i][j] = seed[j];
             }
-            else {
+            else
+            {
                 board[i][j] = ' ';
                 boardDeepCopy[i][j] = ' ';
             }
@@ -260,7 +309,7 @@ int main() {
     }
 
     // fill out board, and a solution board with all values present.
-    GenerateBoard game;
+    Board game;
     game.generateSudoku(board);
     game.generateSudoku(boardDeepCopy);
     solutionBoard = boardDeepCopy;
@@ -271,24 +320,30 @@ int main() {
     cin >> clues;
     int clueNums;
 
-    try {
+    try
+    {
         clueNums = stoi(clues);
-        if (clueNums > 80 || clueNums < 7) {
+        if (clueNums > 80 || clueNums < 7)
+        {
             cout << "out of range. Starting game with 7." << endl;
             clueNums = 7;
         }
     }
-    catch (const invalid_argument& e) {
+    catch (const invalid_argument &e)
+    {
         cout << "not a number. Starting game with 7." << endl;
         clueNums = 7;
     }
-    catch (const out_of_range& e) {
+    catch (const out_of_range &e)
+    {
         cout << "out of range. Starting game with 7." << endl;
         clueNums = 7;
     }
 
-    cout << endl << "\033[32mat any point, type 's' to show board solution.\033[0m" << endl << endl;
-    
+    cout << endl
+         << "\033[32mat any point, type 's' to show board solution.\033[0m" << endl
+         << endl;
+
     vector<vector<bool>> startingPositions = generateStartPos(clueNums);
 
     // render in console
@@ -296,33 +351,39 @@ int main() {
 
     // actual game loop
     bool notFinished = true;
-    while (true) {
-        bool validTurn = takeATurn(board,startingPositions);
-        if (!validTurn) {
+    while (true)
+    {
+        bool validTurn = takeATurn(board, startingPositions);
+        if (!validTurn)
+        {
             // re-render
             game.Render(board, startingPositions, false);
             continue;
         }
         // valid turn here
-        else {
-            for (int q = 0; q < board.size(); q++){
-                if (solutionBoard[q] != board[q]) {
+        else
+        {
+            for (int q = 0; q < board.size(); q++)
+            {
+                if (solutionBoard[q] != board[q])
+                {
                     notFinished = true;
                     break;
                 }
                 notFinished = false;
             }
-            if (notFinished) {
+            if (notFinished)
+            {
                 game.Render(board, startingPositions, false);
                 continue;
             }
             // endgame
-            else {
+            else
+            {
                 game.Render(board, startingPositions, false);
                 cout << "\033[32mBoard Solved.\033[0m" << endl;
                 break;
             }
-            
         }
     }
 }
